70039e27a2d09cc46309d08b00d12bc3efb4f4a4
Muhammad Afzal <134699152+AfzalAheer@users.noreply.github.com>
2024-10-31 13:31:04 -0700

    Create script.js
    added code for script

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+
+function toggleMenu() {
+    const navMenu = document.getElementById('navMenu');
+    navMenu.classList.toggle('active'); // Toggle 'active' class to show/hide menu
+}
